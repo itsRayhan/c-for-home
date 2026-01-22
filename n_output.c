c6b3ce5bce2d913b28443c202ea20cda7a8e4b4c
itsRayhan <itsrayhaninbox@gmail.com>
2026-01-22 22:26:46 +0600

    22

---
+++ n_output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d", &n);
+
+    for(i=0;i<=n;i++)
+    {
+        printf("your need %d\n", i);
+    }
+
+
+    printf("");
+    return 0;
+}
