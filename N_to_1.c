906c79b886af93b1a53c463f7e334920bd3fcdf7
itsRayhan <itsrayhaninbox@gmail.com>
2026-01-22 22:37:08 +0600

    22/01/2026

--- N_to_1.c
+++ N_to_1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int i, n;
-    scanf("%d", &n );
+int main()
+{
+    int n;
+    scanf("%d", &n);
 
-    for(i=0;i>n;i--)
+    for (int i = n; i >= 1; i--)
     {
-        printf("Back to =%d\n",i);
+        printf("Back to =%d\n", i);
     }
-    //printf("rayhan=%d\n",i);
+    // printf("rayhan=%d\n",i);
     return 0;
-}
+}    
