25309e1b7658649f0efb58128224604f48e72dce
itsRayhan <itsrayhaninbox@gmail.com>
2026-02-01 22:07:58 +0600

    versity 1

--- 1_29_2026.c
+++ 1_29_2026.c
@@ -4,10 +4,19 @@ int main() {
     int arry[]= {12,32,43,84,1,54,6,15,87,64};
     int n;
 
-    for( );
-    
-    printf("");
+    //normal order print
+    printf("normal order:");
+    for (n = 0; n <= 9; n++)
+    {
+        printf(" %d  ", arry[n]);
+    }
 
+    // revrse order
+    printf("reverse order:");
+    for (n = 9; n >= 0; n--)
+    {
+        printf(" %d  ", arry[n]);
+    }
 
-    return 0;
-}2
+        return 0;
+}
